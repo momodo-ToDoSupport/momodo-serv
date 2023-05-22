6399d8bca2713cf98c82fc26e87a545a51e86af8
soohyunnn <sksmsqodn20@gmail.com>
2023-05-22 20:18:24 +0900

    first commit

---
+++ src/main/java/com/momodo/MomodoServApplication.java
@@ -0,0 +1,13 @@
+package com.momodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomodoServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomodoServApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/momodo/MomodoServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomodoServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
