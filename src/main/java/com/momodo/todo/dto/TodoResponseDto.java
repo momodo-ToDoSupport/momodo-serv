34b4774f8bf81265889bd15104a30d347fca46b6
SBSun <46879319+SBSun@users.noreply.github.com>
2023-06-04 18:22:38 +0900

    Feat : TodoResponseDto 생성

---
+++ src/main/java/com/momodo/todo/dto/TodoResponseDto.java
@@ -0,0 +1,44 @@
+package com.momodo.todo.dto;
+
+import com.momodo.todo.Todo;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+public class TodoResponseDto {
+
+    @Getter
+    @NoArgsConstructor
+    public static class Info{
+
+        private Long id;
+        private String title;
+        private String emoji;
+        private LocalDate dueDate;
+        private boolean isCompleted;
+        private String repeatDays;
+
+        @Builder
+        public Info(Long id, String title, String emoji, LocalDate dueDate, boolean isCompleted, String repeatDays) {
+            this.id = id;
+            this.title = title;
+            this.emoji = emoji;
+            this.dueDate = dueDate;
+            this.isCompleted = isCompleted;
+            this.repeatDays = repeatDays;
+        }
+
+        public Info toInfo(Todo todo){
+            return Info.builder()
+                    .id(id)
+                    .title(title)
+                    .emoji(emoji)
+                    .dueDate(dueDate)
+                    .isCompleted(isCompleted)
+                    .repeatDays(repeatDays)
+                    .build();
+        }
+    }
+}
